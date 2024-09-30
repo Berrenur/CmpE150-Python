45395b7310ff1d43e0663117092676e3375e7d75
Berrenur <berrenursaylam@hotmail.com>
2024-09-30 22:23:58 +0300

    Update and rename README.md to questions.py

---
+++ Fall24/week01/questions.py
@@ -0,0 +1,48 @@
+## Question 1: Chatbot
+
+#Chatbot: Hello there! I’m the CS101 Study Bot. What’s your name?
+#User: Ayse
+#Chatbot: Great to meet you, Ayse! How many semesters have you been studying Computer Science?
+#User: 2
+#Chatbot: Ah, “2” semesters, nice! I’m still learning myself. What are you working on right now?
+#User: Learning about algorithms.
+#Chatbot: “Algorithms”, huh? That’s a challenging topic! I can’t say I know all the algorithms yet, but maybe we can figure it out together? Need help with something specific?
+#User: Can you explain sorting algorithms?
+#Chatbot: I wish I could, but I’m just a simple chatbot for now. Maybe check out the lecture notes or ask your instructor for more details. I’m here for small talk though! Anything else?
+#User: Tell me more about you.
+#Chatbot: Well, I was programmed by “Ayse” in a few lines of code for CS101. My job is to chat and keep you company while you study. Have fun with your course, and let’s talk again soon!
+
+
+
+
+
+## Question 2: 
+
+# You are expected to display each one of the following messages on a new line
+# "Cmpe150 requires regular practice"
+# "The more I study, the easier it will become"
+#
+# After these two messages, you should define a function calculate_f to calculate
+# and display the result of f = m * a for the given m and a arguments.
+# Then you should define getWidth function to return the width of a triangle as explained
+# in the question description
+
+
+# DO_NOT_EDIT_ANYTHING_ABOVE_THIS_LINE
+
+
+
+# DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE
+
+m = int(input())
+a = int(input())
+calculate_f(m, a)
+
+triangle_height = int(input())
+triangle_width = getWidth(triangle_height)
+print(triangle_width)
+
+
+
+
+
